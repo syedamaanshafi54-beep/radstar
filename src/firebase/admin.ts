1683081809f8f1df4921f4773ce840cce891a8cd
Your Name <itsmeabdulk@gmail.com>
2025-12-06 08:47:57 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,4 +1,4 @@
-'use server';
+
 import { App, getApp, getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
